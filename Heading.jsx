d3155fb0449ddec42612c005419e8cf9ee32aa7c
Varshabang <57285793+Varshabang@users.noreply.github.com>
2021-06-06 23:30:38 +0530

    Add files via upload

---
+++ Heading.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Heading(){
+  return (<header>
+    <h1> SHAPE AI </h1>
+  </header>);
+}
+export default Heading;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1> Javascript and React.js </h1>
+      <p>
+        {" "}
+        This was and amazing bootcamp taken up by Shaurya Sinha. We covered
+        everything from scratch including javascript , react.js, HTML.
+      </p>
+    </div>
+  );
+}
+export default Note;
